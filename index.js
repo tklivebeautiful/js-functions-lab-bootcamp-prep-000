6b7b8fbbf4f98dc667304e9ec070734c9fe421bd
Tara Kaestner <tara.livebeautiful@gmail.com>
2017-10-02 19:50:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // write your code below!
 function happyHolidays() {
-  console.log ("Happy Holidays!")
+  return ('Happy holidays!')
+}
+function happyHolidaysTo(name) {
+  return (`Happy holidays, ${name}!`)
+}
+function happyHolidayTo(holiday,name) {
+  return (`Happy ${holiday}, ${name}!`)
 }
